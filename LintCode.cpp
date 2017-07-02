9b5b16c6a14bd0fb571df93d36d9472001067a1b
HappyYangShine <zhangyyhi@163.com>
2017-07-02 16:44:34 +0800

    add an answer: longest common subsequence

--- LintCode.cpp
+++ LintCode.cpp
@@ -7046,4 +7046,59 @@ public:
 };
 
 
+77 最长公共子序列 longestCommonSubsequence
+给出两个字符串，找到最长公共子序列(LCS)，返回LCS的长度。
+
+ 说明
+
+最长公共子序列的定义：
+
+    最长公共子序列问题是在一组序列（通常2个）中找到最长公共子序列（注意：不同于子串，LCS不需要是连续的子串）。该问题是典型的计算机科学问题，是文件差异比较程序的基础，在生物信息学中也有所应用。
+    https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+
+样例
+
+给出"ABCD" 和 "EDCA"，这个LCS是 "A" (或 D或C)，返回1
+
+给出 "ABCD" 和 "EACB"，这个LCS是"AC"返回 2
+
+
+http://blog.csdn.net/v_july_v/article/details/6695482
+http://blog.csdn.net/waltonhuang/article/details/52032463
+
+
+class Solution {
+public:
+    /**
+     * @param A, B: Two strings.
+     * @return: The length of longest common subsequence of A and B.
+     */
+    int longestCommonSubsequence(string A, string B) {
+        // write your code here
+        /*
+        if (A.empty() || B.empty())
+        {
+            return 0;
+        }
+        */
+        int dp[A.size() + 1][B.size() + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= A.size(); ++i)
+        {
+            for (int j = 1; j <= B.size(); ++j)
+            {
+                if (A[i - 1] == B[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i -1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[A.size()][B.size()];
+    }
+};
 
